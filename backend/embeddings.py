bcc3e7a8335a60fe2e7072ceb1baad77dec6b165
ItsPoonamP <147233308+ItsPoonamP@users.noreply.github.com>
2026-01-17 11:16:27 +0530

    Refactor embedding function to use new client

--- backend/embeddings.py
+++ backend/embeddings.py
@@ -1,21 +1,24 @@
+from google import genai
 import os
-import google.generativeai as genai
+
+client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
 from dotenv import load_dotenv
 
 load_dotenv()
 
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
 
 
 def embed_text(text: str):
     """
     Returns an embedding vector using Gemini
     """
-    response = genai.embed_content(
+    response = client.models.embed_content(
         model="text-embedding-004",
         content=text
     )
     return response["embedding"]
+
 
 
 
